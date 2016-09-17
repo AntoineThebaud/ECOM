de999ffbcf6addfbc96107b46f8d592540f13451
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-09-17 20:25:31 +0200

    petit push du samedi
    - script SQL pour insérer des instruments.
    - amélioration affichage liste de produits
    - nom de commit tout pérave.

---
+++ DB/insertInstru.sql
@@ -0,0 +1,6 @@
+# 2 = best seller
+# 3 = promo (10%)
+
+INSERT INTO instrument VALUES (1, 0, 666, 'Fender', 'FenderSquier.png', 'Squier Bullet Strat RW BK', 3.756, 125, 0, 'Guitare');
+INSERT INTO instrument VALUES (2, 1, 666, 'Fender', 'electric-guitar-strat.jpg', 'SQ Standard Strat FMT AMB', 3.756, 319, 0, 'Guitare');
+INSERT INTO instrument VALUES (3, 0, 666, 'Fender', 'FenderSquier.png', 'Squier Telecaster', 3.756, 450, 10, 'Guitare');
