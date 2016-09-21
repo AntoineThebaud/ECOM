25360f182a82da7c4600895fc3304ce92f89601f
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-09-21 23:43:04 +0200

    Navbar : sous-sous menus OK
    - arborescence de menu complétée avec les valeurs définitives (ou pas ?)
    pour les guitares.
    - "quand ça marche, c'est bien" - un con, 2016.

--- Projet/HarmonixWeb/WebContent/shared/navbar/navbarCtrl.js
+++ Projet/HarmonixWeb/WebContent/shared/navbar/navbarCtrl.js
@@ -5,28 +5,50 @@
 	
 	module.controller('navbarController', function(Panier) {
 		var vm = this;
-		vm.index = null;
+		
 		vm.displayChilds = function(index) {
-			vm.index = index;
 	        vm.items[index].active = !vm.items[index].active;
 	    };
 	    
-	    vm.displayChildsOfChilds = function(index) {
-	    	vm.subItems[index].active = !vm.subItems[index].active;
+	    vm.displayChildsOfChilds = function(index, parentIndex) {   	
+	    	vm.items[parentIndex].subItems[index].active = !vm.items[parentIndex].subItems[index].active;
 	    };
 	    
 	    vm.items = [
             {
                 name: "Guitares",
                 subItems: [
-                    {name: "Guitares acoustiques",
-                    subSubItems: [
-                                  {name: "Guitares classiques"},
-                                  {name: "Guitares folk"}
-                    ]
-                    },
-                    {name: "Guitares électriques"},
-                    {name: "Guitares électro-acoustiques"}
+					{name: "Guitares électriques",
+						subSubItems: [
+							{name: "Modèles ST"},
+							{name: "Modèles T"},
+							{name: "Modèles Single Cut"},
+							{name: "Modèles Double Cut"},
+							{name: "Guitares Metal"},
+							{name: "Guitares 7 Cordes"},
+							{name: "Guitares 8 Cordes"}
+						]
+					},
+					{name: "Guitares classiques",
+						subSubItems: [
+					      {name: "Guitares classiques 1/4"},
+					      {name: "Guitares classiques 1/2"},
+					      {name: "Guitares classiques 3/4"},
+					      {name: "Guitares classiques 7/8"},
+					      {name: "Guitares classiques 4/4"},
+					      {name: "Guitares classiques électro-acoustiques"}
+					    ]
+					},
+                    {name: "Guitares Folk",
+                    	subSubItems: [
+                          {name: "Guitares Dreadnought"},
+                          {name: "Guitares Jumbo"},
+                          {name: "Guitares 000"},
+                          {name: "Guitares Dreadnought électro-acoustiques"},
+                          {name: "Guitares Jumbo électro-acoustiques"},
+                          {name: "Guitares 000 électro-acoustiques"}
+                        ]
+                    }
                 ]
             },
             {
@@ -44,14 +66,5 @@
                 ]
             }
         ];
-	    
-	   vm.subItems = [
-          {name: "Guitares acoustiques",
-			subSubItems: [
-                  {name: "Guitares classiques"},
-                  {name: "Guitares folk"}
-              	]
-			}
-	    ]; 
 	});
 })();
