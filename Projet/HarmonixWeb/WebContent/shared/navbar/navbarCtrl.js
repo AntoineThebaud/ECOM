f5e356f3c0eda2a6e318ddcabdd260877732fb66
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-09-22 09:40:36 +0200

    Navbar : liens OK
    - ajout d'un attribut url contenant une chaine formatée correctement
    (underscores, pas d'accents, etc..)

--- Projet/HarmonixWeb/WebContent/shared/navbar/navbarCtrl.js
+++ Projet/HarmonixWeb/WebContent/shared/navbar/navbarCtrl.js
@@ -17,36 +17,43 @@
 	    vm.items = [
             {
                 name: "Guitares",
+                url: "guitares",
                 subItems: [
-					{name: "Guitares électriques",
+					{
+						name: "Guitares électriques",
+						url: "guitares_electriques",
 						subSubItems: [
-							{name: "Modèles ST"},
-							{name: "Modèles T"},
-							{name: "Modèles Single Cut"},
-							{name: "Modèles Double Cut"},
-							{name: "Guitares Metal"},
-							{name: "Guitares 7 Cordes"},
-							{name: "Guitares 8 Cordes"}
+							{name: "Modèles ST", url: "modeles_st"},
+							{name: "Modèles T", url: "modeles_t"},
+							{name: "Modèles Single Cut", url: "modeles_single_cut"},
+							{name: "Modèles Double Cut", url: "modeles_double_cut"},
+							{name: "Guitares Metal", url: "guitares_metal"},
+							{name: "Guitares 7 Cordes", url: "guitares_7_cordes"},
+							{name: "Guitares 8 Cordes", url: "guitares_8_cordes"}
 						]
 					},
-					{name: "Guitares classiques",
+					{	
+						name: "Guitares classiques",
+						url: "guitares_classiques",
 						subSubItems: [
-					      {name: "Guitares classiques 1/4"},
-					      {name: "Guitares classiques 1/2"},
-					      {name: "Guitares classiques 3/4"},
-					      {name: "Guitares classiques 7/8"},
-					      {name: "Guitares classiques 4/4"},
-					      {name: "Guitares classiques électro-acoustiques"}
+					      {name: "Guitares classiques 1/4", url: "guitares_classiques_1_4"},
+					      {name: "Guitares classiques 1/2", url: "guitares_classiques_1_2"},
+					      {name: "Guitares classiques 3/4", url: "guitares_classiques_3_4"},
+					      {name: "Guitares classiques 7/8", url: "guitares_classiques_7_8"},
+					      {name: "Guitares classiques 4/4", url: "guitares_classiques_4_4"},
+					      {name: "Guitares classiques électro-acoustiques", url: "guitares_classiques_electro_acoustiques"}
 					    ]
 					},
-                    {name: "Guitares Folk",
+                    {
+						name: "Guitares Folk",
+						url: "guitares_folk",
                     	subSubItems: [
-                          {name: "Guitares Dreadnought"},
-                          {name: "Guitares Dreadnought électro-acoustiques"},
-                          {name: "Guitares Jumbo"},
-                          {name: "Guitares Jumbo électro-acoustiques"},
-                          {name: "Guitares 000"},
-                          {name: "Guitares 000 électro-acoustiques"}
+                          {name: "Guitares Dreadnought", url: "guitares_dreadnought"},
+                          {name: "Guitares Dreadnought électro-acoustiques", url: "guitares_dreadnought_electro_acoustiques"},
+                          {name: "Guitares Jumbo", url: "guitares_jumbo"},
+                          {name: "Guitares Jumbo électro-acoustiques", url: "guitares_jumbo_electro_acoustiques"},
+                          {name: "Guitares 000", url: "guitares_000"},
+                          {name: "Guitares 000 électro-acoustiques", url: "guitares_000_electro_acoustiques"}
                         ]
                     }
                 ]
