a15f0e5c734c12b667cbdf15d904ea455fa1c442
hamdiMo <medfsm@gmail.com>
2016-09-23 11:45:34 +0200

    formulaire d'inscription : contrôle de saisie OK

--- Projet/HarmonixWeb/WebContent/components/utilisateur/utilisateurCtrl.js
+++ Projet/HarmonixWeb/WebContent/components/utilisateur/utilisateurCtrl.js
@@ -1,26 +1,33 @@
-(function () {
+(function() {
 	"use strict";
 
 	var module = angular.module("app");
-	module.controller('UtilisateurController',  function(Utilisateur) {
+	module.controller('UtilisateurController', function(Utilisateur) {
 		var vm = this;
 		var utilisateur = new Utilisateur();
-		
+
 		vm.utilisateur = {
-				nom : '', 
-				prenom : '',
-				age : '',
-				mail : '',
-				motDePasse : '',
-					};
+			nom : '',
+			prenom : '',
+			age : '',
+			mail : '',
+			motDePasse : '',
+		};
 		vm.ajoutClient = function() {
 			Utilisateur.save(vm.utilisateur, function() {
 				console.log('ajout réussi')
-				  //data saved. $scope.entry is sent as the post body.
+				// data saved. $scope.entry is sent as the post body.
 			});
-			
+
 		}
-		 
-		
+		vm.verifMail = function() {
+			var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+			if (!regex.test(vm.utilisateur.mail)) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
 	});
 })();
