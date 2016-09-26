3fc946198ac12471b024cfac6acc06cf2e1d8a80
LilyOfTheWest <linovergame@hotmail.fr>
2016-09-26 14:34:45 +0200

    Ben en fait il restait des chemins absolus :-)

--- Projet/HarmonixWeb/WebContent/shared/instrument/instrumentFact.js
+++ Projet/HarmonixWeb/WebContent/shared/instrument/instrumentFact.js
@@ -4,7 +4,7 @@
 	var module = angular.module("app");//retrieve the module named "app"
 	
 	module.factory('Instrument', function ($resource) {
-		return $resource('http://localhost:8080/HarmonixWeb/rest/instruments/:id', { id: '@_id' }, {
+		return $resource('rest/instruments/:id', { id: '@_id' }, {
 			update: {
 				method: 'PUT' // this method issues a PUT request
 			}
--- Projet/HarmonixWeb/WebContent/shared/utilisateur/utilisateurFact.js
+++ Projet/HarmonixWeb/WebContent/shared/utilisateur/utilisateurFact.js
@@ -4,7 +4,7 @@
 	var module = angular.module("app");//retrieve the module named "app"
 	
 	module.factory('Utilisateur', function ($resource) {
-		return $resource('http://localhost:8080/HarmonixWeb/rest/utilisateurs/:mail/:password', {
+		return $resource('rest/utilisateurs/:mail/:password', {
 			mail: '@mail',
 			password: '@password'
 		}, {
