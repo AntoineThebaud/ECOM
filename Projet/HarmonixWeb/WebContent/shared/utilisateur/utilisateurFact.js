d866f5cbf3b5f9269ffc868bc7051f66c10857ed
hamdiMo <medfsm@gmail.com>
2016-09-28 19:02:05 +0200

    correctif connexion et inscription

--- Projet/HarmonixWeb/WebContent/shared/utilisateur/utilisateurFact.js
+++ Projet/HarmonixWeb/WebContent/shared/utilisateur/utilisateurFact.js
@@ -4,6 +4,12 @@
 	var module = angular.module("app");//retrieve the module named "app"
 	
 	module.factory('Utilisateur', function ($resource) {
+		return $resource('../../rest/utilisateurs/:id', { id: '@_id',}, {
+		    stripTrailingSlashes: false
+		});
+	});
+	
+	module.factory('Auth', function ($resource) {
 		return $resource('rest/utilisateurs/:mail/:password', {
 			mail: '@mail',
 			password: '@password'
@@ -11,4 +17,5 @@
 		    stripTrailingSlashes: false
 		});
 	});
+	
 })();
