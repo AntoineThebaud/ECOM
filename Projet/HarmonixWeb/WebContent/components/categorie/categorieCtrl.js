53246e34fb8167030aab23b51853969b38ed42ba
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-09-28 17:24:06 +0200

    Filtre par prix + Filtre par note OK
    - barre de filtre fonctionnel sur toutes les pages concernées
    (catégorie, promotions, nouveautés) => /!\ duplication de code à revoir
    si possible
    - le javascript pour les tris c'est cool.
    - j'ai mis 2 "t" à littoral (tout le monde s'en fout)

--- Projet/HarmonixWeb/WebContent/components/categorie/categorieCtrl.js
+++ Projet/HarmonixWeb/WebContent/components/categorie/categorieCtrl.js
@@ -8,7 +8,9 @@
 		vm.instruments = [];
 		vm.displayedInstruments = [];
 		
-		//Filtre par marque : génerère le contenu du select //TODO : code duppliqué (catégorie, promos)
+		//******************* code duppliqué (catégorie, promotions, nouveautés) *********************/
+		
+		//Filtre par marque : génerère le contenu du select 
 		vm.getCurrentMarques = function() {
 			var tab = [];
 			var brand;
@@ -25,21 +27,62 @@
 		vm.displayBrand = function(selected_brand) {
 			//réinitialisation du tableau avant traitement
 			vm.displayedInstruments = vm.instruments.slice();
-			if(selected_brand == "Toutes les marques") return;	
-			for(var i = 0; i < vm.displayedInstruments.length; i++) {
-				if(vm.displayedInstruments[i].fabricant != selected_brand) {
-					vm.displayedInstruments.splice(i,1);
-					--i;
+			if(selected_brand != "Toutes les marques") {
+				for(var i = 0; i < vm.displayedInstruments.length; i++) {
+					if(vm.displayedInstruments[i].fabricant != selected_brand) {
+						vm.displayedInstruments.splice(i,1);
+						--i;
+					}
 				}
 			}
+			//recharger l'autre tri sélectionné si il y en a
+			if ($scope.selected_sort_price != "Filtrer par prix") {
+				vm.displayByPrice($scope.selected_sort_price);
+			} else if ($scope.selected_sort_note != "Filtrer par note") {
+				vm.displayByNote($scope.selected_sort_note);
+			}
 		}
+		
+		//Filtre par prix : croissant ou décroissant
+		vm.displayByPrice = function (selected_sort_price) {
+			if(selected_sort_price == "Du moins cher au plus cher.") {
+				vm.displayedInstruments.sort(function(a, b) {
+				    return parseFloat(a.prix * ((100-a.promo)/100)) - parseFloat(b.prix* ((100-b.promo)/100));
+				});	
+			} else if(selected_sort_price == "Du plus cher au moins cher."){
+				vm.displayedInstruments.sort(function(a, b) {
+				    return parseFloat(b.prix * ((100-b.promo)/100)) - parseFloat(a.prix* ((100-a.promo)/100));
+				});
+			}
+			//réinit filtre note
+			$scope.selected_sort_note = "Filtrer par note";
+		}
+		
+		//Filtrer par note : croissant ou décroissant
+		vm.displayByNote = function (selected_sort_note) {
+			if(selected_sort_note == "Du moins bien noté au mieux noté.") {
+				vm.displayedInstruments.sort(function(a, b) {
+				    return parseFloat(a.note) - parseFloat(b.note);
+				});	
+			} else if(selected_sort_note == "Du mieux noté au moins bien noté."){
+				vm.displayedInstruments.sort(function(a, b) {
+					return parseFloat(b.note) - parseFloat(a.note);
+				});
+			}
+			//réinit filtre prix
+			$scope.selected_sort_price = "Filtrer par prix";
+		}
+		
+		//********************************************************************************************/
 
 		var getInstruments = Instrument.query({categorie: $routeParams.TYPE}, function() {
 			vm.instruments = getInstruments;
 			vm.displayedInstruments = vm.instruments.slice();
-			//init filtre marque
+			//init filtres
 			vm.marques = vm.getCurrentMarques();
-			$scope.selected_brand = "Toutes les marques";
+			$scope.selected_brand = "Filtrer par marque";
+			$scope.selected_sort_price = "Filtrer par prix";
+			$scope.selected_sort_note = "Filtrer par note";
 		});
 		
 		vm.categoriesFormat = {
--- Projet/HarmonixWeb/WebContent/components/nouveautes/nouveautesCtrl.js
+++ Projet/HarmonixWeb/WebContent/components/nouveautes/nouveautesCtrl.js
@@ -7,14 +7,16 @@
 		var vm = this;
 		vm.instruments = [];
 		
-		//Filtre par marque : génerère le contenu du select //TODO : code duppliqué (catégorie, promos)
+//******************* code duppliqué (catégorie, promotions, nouveautés) *********************/
+		
+		//Filtre par marque : génerère le contenu du select 
 		vm.getCurrentMarques = function() {
 			var tab = [];
 			var brand;
 			for(var i = 0; i < vm.instruments.length; ++i) {
 				brand = vm.instruments[i].fabricant;
 				if(tab.indexOf(brand) == -1) {
-					tab.push(vm.instruments[i].fabricant);
+					tab.push(brand);
 				}				
 			}
 			return tab;
@@ -24,21 +26,62 @@
 		vm.displayBrand = function(selected_brand) {
 			//réinitialisation du tableau avant traitement
 			vm.displayedInstruments = vm.instruments.slice();
-			if(selected_brand == "Toutes les marques") return;	
-			for(var i = 0; i < vm.displayedInstruments.length; i++) {
-				if(vm.displayedInstruments[i].fabricant != selected_brand) {
-					vm.displayedInstruments.splice(i,1);
-					--i;
+			if(selected_brand != "Toutes les marques") {
+				for(var i = 0; i < vm.displayedInstruments.length; i++) {
+					if(vm.displayedInstruments[i].fabricant != selected_brand) {
+						vm.displayedInstruments.splice(i,1);
+						--i;
+					}
 				}
 			}
+			//recharger l'autre tri sélectionné si il y en a
+			if ($scope.selected_sort_price != "Filtrer par prix") {
+				vm.displayByPrice($scope.selected_sort_price);
+			} else if ($scope.selected_sort_note != "Filtrer par note") {
+				vm.displayByNote($scope.selected_sort_note);
+			}
 		}
+		
+		//Filtre par prix : croissant ou décroissant
+		vm.displayByPrice = function (selected_sort_price) {
+			if(selected_sort_price == "Du moins cher au plus cher.") {
+				vm.displayedInstruments.sort(function(a, b) {
+				    return parseFloat(a.prix * ((100-a.promo)/100)) - parseFloat(b.prix* ((100-b.promo)/100));
+				});	
+			} else if(selected_sort_price == "Du plus cher au moins cher."){
+				vm.displayedInstruments.sort(function(a, b) {
+				    return parseFloat(b.prix * ((100-b.promo)/100)) - parseFloat(a.prix* ((100-a.promo)/100));
+				});
+			}
+			//réinit filtre note
+			$scope.selected_sort_note = "Filtrer par note";
+		}
+		
+		//Filtrer par note : croissant ou décroissant
+		vm.displayByNote = function (selected_sort_note) {
+			if(selected_sort_note == "Du moins bien noté au mieux noté.") {
+				vm.displayedInstruments.sort(function(a, b) {
+				    return parseFloat(a.note) - parseFloat(b.note);
+				});	
+			} else if(selected_sort_note == "Du mieux noté au moins bien noté."){
+				vm.displayedInstruments.sort(function(a, b) {
+					return parseFloat(b.note) - parseFloat(a.note);
+				});
+			}
+			//réinit filtre prix
+			$scope.selected_sort_price = "Filtrer par prix";
+		}
+		
+		//********************************************************************************************/
 
 		var getNouveautes = Instrument.query({id: "nouveautes"}, function() {
 			vm.instruments = getNouveautes;
 			vm.displayedInstruments = vm.instruments.slice();
-			//init filtre marque
+			//init filtres
 			vm.marques = vm.getCurrentMarques();
-			$scope.selected_brand = "Toutes les marques";
+			$scope.selected_brand = "Filtrer par marque";
+			$scope.selected_sort_price = "Filtrer par prix";
+			$scope.selected_sort_note = "Filtrer par note";
 		});
 		
 		//fonction pour affichage etoiles //TODO : code duppliqué (promos, catégorie, ficheProduit)
