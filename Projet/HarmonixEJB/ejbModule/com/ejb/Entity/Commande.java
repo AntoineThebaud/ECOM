3a89830cbe2d51f02ef21f477ff4e502eda506b7
Mickaël Turnel <mickael.turnel@gmail.com>
2016-09-28 22:39:51 +0200

    Ajout de mail et de la requête préparée de listage

--- Projet/HarmonixEJB/ejbModule/com/ejb/Entity/Commande.java
+++ Projet/HarmonixEJB/ejbModule/com/ejb/Entity/Commande.java
@@ -12,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -20,6 +22,9 @@ import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name = "allCommandes", query = "SELECT OBJECT(c) FROM Commande c ORDER BY c.dateCommande DESC")
+})
 public class Commande {
 
 	@Id
@@ -33,6 +38,10 @@ public class Commande {
 
 	private int etat;
 	
+	private String mail;
+	
+	// TODO LES ADRESSES
+	
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_commande", referencedColumnName = "id_commande")
 	@Fetch(value = FetchMode.SUBSELECT)
@@ -43,11 +52,12 @@ public class Commande {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Commande(Date date, int etat, Collection<LigneCommande> ligneCommande) {
+	public Commande(Date date, int etat, String mail, Collection<LigneCommande> ligneCommande) {
 		super();
 
 		this.dateCommande = date;
 		this.etat = etat;
+		this.mail = mail;
 		this.ligneCommande = ligneCommande;
 	}
 
--- Projet/HarmonixEJB/ejbModule/com/ejb/Entity/Commande_.java
+++ Projet/HarmonixEJB/ejbModule/com/ejb/Entity/Commande_.java
@@ -6,11 +6,12 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-09-27T14:32:44.292+0200")
+@Generated(value="Dali", date="2016-09-28T22:03:47.165+0200")
 @StaticMetamodel(Commande.class)
 public class Commande_ {
 	public static volatile SingularAttribute<Commande, Long> idCommande;
 	public static volatile SingularAttribute<Commande, Date> dateCommande;
 	public static volatile SingularAttribute<Commande, Integer> etat;
 	public static volatile CollectionAttribute<Commande, LigneCommande> ligneCommande;
+	public static volatile SingularAttribute<Commande, String> mail;
 }
