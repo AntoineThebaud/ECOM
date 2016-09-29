2ed9236aa99aedbf336a9edfa338bab8c358ba92
Mickaël Turnel <mickael.turnel@gmail.com>
2016-09-30 00:23:20 +0200

    Changement requête meilleures ventes

--- Projet/HarmonixEJB/ejbModule/com/ejb/Entity/Commande_.java
+++ Projet/HarmonixEJB/ejbModule/com/ejb/Entity/Commande_.java
@@ -6,12 +6,12 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-09-28T22:03:47.165+0200")
+@Generated(value="Dali", date="2016-09-30T00:03:01.642+0200")
 @StaticMetamodel(Commande.class)
 public class Commande_ {
 	public static volatile SingularAttribute<Commande, Long> idCommande;
 	public static volatile SingularAttribute<Commande, Date> dateCommande;
 	public static volatile SingularAttribute<Commande, Integer> etat;
-	public static volatile CollectionAttribute<Commande, LigneCommande> ligneCommande;
 	public static volatile SingularAttribute<Commande, String> mail;
+	public static volatile CollectionAttribute<Commande, LigneCommande> ligneCommande;
 }
--- Projet/HarmonixEJB/ejbModule/com/ejb/Entity/LigneCommande_.java
+++ Projet/HarmonixEJB/ejbModule/com/ejb/Entity/LigneCommande_.java
@@ -4,10 +4,10 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-09-28T22:59:19.871+0200")
+@Generated(value="Dali", date="2016-09-30T00:03:01.695+0200")
 @StaticMetamodel(LigneCommande.class)
 public class LigneCommande_ {
 	public static volatile SingularAttribute<LigneCommande, Long> idLigneCommande;
-	public static volatile SingularAttribute<LigneCommande, Integer> quantite;
 	public static volatile SingularAttribute<LigneCommande, Long> idInstrument;
+	public static volatile SingularAttribute<LigneCommande, Integer> quantite;
 }
