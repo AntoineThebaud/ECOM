08c98408b630c6fa10934c10029a674c5986fb49
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-09-30 13:40:06 +0200

    localhost dans bblabla part. 2

--- Projet/HarmonixWeb/WebContent/shared/commande/commandeFact.js
+++ Projet/HarmonixWeb/WebContent/shared/commande/commandeFact.js
@@ -4,6 +4,7 @@
 	var module = angular.module("app");//retrieve the module named "app"
 	
 	module.factory('Commande', function ($resource) {
+		return $resource('../../rest/commandes/:id', { id: '@_id' }, {
 			update: {
 				method: 'PUT' // this method issues a PUT request
 			}
